cbcacd2b42d0066f0d4b028bc6ad7e27dd392e38
Julien Roth <julien@infraroth.com>
2021-11-25 23:20:09 +0100

    Updated player and song csv

--- src/components/Lecteur/Lecteur.jsx
+++ src/components/Lecteur/Lecteur.jsx
@@ -1,12 +1,18 @@
-import ReactAudioPlayer from "react-audio-player";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 import "./Lecteur.css";
 
 const Lecteur = () => {
-    return (
-        <div>
-            <ReactAudioPlayer src="my_audio_file.ogg" autoPlay controls />
-        </div>
-    );
+  return (
+    <div>
+      <AudioPlayer
+        autoPlay
+        src="https://aveclagare.org/mp3/120717RisingAppalachia-Wider%20Circles.mp3"
+        onPlay={(e) => console.log("onPlay")}
+        // other props here
+      />
+    </div>
+  );
 };
 
 export default Lecteur;
