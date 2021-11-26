0594a23e0daad5d7e9d647f36fcb1e56bdc8c9a6
Vincent <cottalorda.vincent@gmail.com>
2021-11-26 04:04:21 +0100

    modif lecteur, fignolages etc

--- src/components/GenreBouton/GenreBouton.jsx
+++ src/components/GenreBouton/GenreBouton.jsx
@@ -1,23 +1,20 @@
-import { useState } from "react";
 import "./genreBouton.css";
-import Pop from "../../assets/pop.mp3"
-import Rock from "../../assets/rock.mp3"
-import Rap from "../../assets/rap.mp3"
-import Rnb from "../../assets/rnb.mp3"
-import Dance from "../../assets/dance.mp3"
-import Variete from "../../assets/variete.mp3"
-import Ambient from "../../assets/ambient.mp3"
-import Funk from "../../assets/funk.mp3"
-import Classique from "../../assets/classique.mp3"
+import Pop from "../../assets/audio/pop.mp3";
+import Rock from "../../assets/audio/rock.mp3";
+import Rap from "../../assets/audio/rap.mp3";
+import Rnb from "../../assets/audio/rnb.mp3";
+import Dance from "../../assets/audio/dance.mp3";
+import Variete from "../../assets/audio/variete.mp3";
+import Ambient from "../../assets/audio/ambient.mp3";
+import Funk from "../../assets/audio/funk.mp3";
+import Classique from "../../assets/audio/classique.mp3";
 
 const GenreBouton = () => {
-const [audio, setAudio] = useState(false);
-  
-  const playSongPop = () => {
-    const song = new Audio();
-    song.src = Pop;
-    song.play();
-  }
+    const playSongPop = () => {
+        const song = new Audio();
+        song.src = Pop;
+        song.play();
+    };
 
     const playSongRock = () => {
         const song = new Audio();
@@ -25,75 +22,93 @@ const [audio, setAudio] = useState(false);
         song.play();
     };
 
-  const playSongRap = () => {
-    const song = new Audio();
-    song.src = Rap;
-    song.play();
-  };
+    const playSongRap = () => {
+        const song = new Audio();
+        song.src = Rap;
+        song.play();
+    };
 
-  const playSongRnb = () => {
-    const song = new Audio();
-    song.src = Rnb;
-    song.play();
-  };
+    const playSongRnb = () => {
+        const song = new Audio();
+        song.src = Rnb;
+        song.play();
+    };
 
-  const playSongDance = () => {
-    const song = new Audio();
-    song.src = Dance;
-    song.play();
-  };
+    const playSongDance = () => {
+        const song = new Audio();
+        song.src = Dance;
+        song.play();
+    };
 
-  const playSongVariete = () => {
-    const song = new Audio();
-    song.src = Variete;
-    song.play();
-  };
+    const playSongVariete = () => {
+        const song = new Audio();
+        song.src = Variete;
+        song.play();
+    };
 
-  const playSongAmbient = () => {
-    const song = new Audio();
-    song.src = Ambient;
-    song.play();
-  };
+    const playSongAmbient = () => {
+        const song = new Audio();
+        song.src = Ambient;
+        song.play();
+    };
 
-  const playSongFunk = () => {
-    const song = new Audio();
-    song.src = Funk;
-    song.play();
-  };
+    const playSongFunk = () => {
+        const song = new Audio();
+        song.src = Funk;
+        song.play();
+    };
 
-  const playSongClassique = () => {
-    const song = new Audio();
-    song.src = Classique;
-    song.play();
-  };
+    const playSongClassique = () => {
+        const song = new Audio();
+        song.src = Classique;
+        song.play();
+    };
 
-  return(
-  <div className='allcontainerbouton'>
-    <div className='containerGenreBouton'>
-     <button className='genreBouton' onClick={playSongPop}>Pop</button>
-     <button  className='butonGenreRock' onClick={playSongRock}>Rock{audio}</button>
-    </div>
+    return (
+        <div className="allcontainerbouton">
+            <div className="containerGenreBouton">
+                <button className="genreBouton" onClick={playSongPop}>
+                    Pop
+                </button>
+                <button className="butonGenreRock" onClick={playSongRock}>
+                    Rock
+                </button>
+            </div>
 
-    <div className='containerGenreBouton'>
-     <button  className='butonGenreRap' onClick={playSongRap}>Rap</button>
-     <button  className='butonGenreRnb' onClick={playSongRnb}>Rnb</button>
-    </div>
+            <div className="containerGenreBouton">
+                <button className="butonGenreRap" onClick={playSongRap}>
+                    Rap
+                </button>
+                <button className="butonGenreRnb" onClick={playSongRnb}>
+                    Rnb
+                </button>
+            </div>
 
-    <div className='containerGenreBouton'>
-     <button  className='butonGenreDance' onClick={playSongDance}>Dance</button>
-     <button  className='butonGenreVariete' onClick={playSongVariete}>Variete</button>
-    </div>
+            <div className="containerGenreBouton">
+                <button className="butonGenreDance" onClick={playSongDance}>
+                    Dance
+                </button>
+                <button className="butonGenreVariete" onClick={playSongVariete}>
+                    Variete
+                </button>
+            </div>
 
-    <div className='containerGenreBouton'>
-     <button  className='butonGenreAmbient' onClick={playSongAmbient}>Ambient</button>
-     <button  className='butonGenreFunk' onClick={playSongFunk}>Funk</button>
-    </div>
- 
-    <div className='containerGenreBoutonclassique'>
-     <button  className='butonGenreClassique' onClick={playSongClassique}>Classique</button> 
-    </div>
-  </div>    
-    )
+            <div className="containerGenreBouton">
+                <button className="butonGenreAmbient" onClick={playSongAmbient}>
+                    Ambient
+                </button>
+                <button className="butonGenreFunk" onClick={playSongFunk}>
+                    Funk
+                </button>
+            </div>
+
+            <div className="containerGenreBoutonclassique">
+                <button className="butonGenreClassique" onClick={playSongClassique}>
+                    Classique
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default GenreBouton;
--- src/components/Lecteur/Lecteur.jsx
+++ src/components/Lecteur/Lecteur.jsx
@@ -2,8 +2,28 @@ import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import "./Lecteur.css";
 
-const Lecteur = ({ playlist }) => {
-    return <AudioPlayer src={playlist[0].url} onPlay={(e) => console.log("onPlay")} />;
+const Lecteur = ({ playlist, current, setCurrent }) => {
+    const prev = () => {
+        if (current > 0) {
+            setCurrent(current - 1);
+        }
+    };
+
+    const next = () => {
+        if (current < playlist.length - 1) {
+            setCurrent(current + 1);
+        }
+    };
+
+    return (
+        <AudioPlayer
+            volume={0.5}
+            onClickPrevious={prev}
+            onClickNext={next}
+            showSkipControls={true}
+            src={playlist[current].url}
+        />
+    );
 };
 
 export default Lecteur;
